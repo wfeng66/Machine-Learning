a3f9d81aecd39e998a77bfc8a1f6d12cfbf31834
Wei Feng <weicongf73@gmail.com>
2021-10-10 09:30:05 -0400

    modified:   CUNY_NLP_CS74040/HW1/model.py

--- CUNY_NLP_CS74040/HW1/model.py
+++ CUNY_NLP_CS74040/HW1/model.py
@@ -13,7 +13,22 @@ class unigram():
                 self.unigramDict[self.tr[i]] = 1
 
 class bigram(unigram):
-    
+    def __init__(self, tr):
+        super(bigram, self).__init__(tr)
+        self.bigramDict = {}
+        #self.biwordList = []
+        self.p = {}
+
+    def fit(self):
+        super(bigram, self).fit()
+        for i in range(len(self.tr)-1):
+            #self.biwordList.append((self.tr[i], self.tr[i+1]))
+            if (self.tr[i], self.tr[i+1]) in self.bigramDict:
+                self.bigramDict[(self.tr[i], self.tr[i+1])] += 1
+            else:
+                self.bigramDict[(self.tr[i], self.tr[i + 1])] = 1
+        for (wd1, wd2), count in self.bigramDict.items():
+            self.p[(wd1, wd2)] = count/self.unigramDict.get(wd1)
 
 
 
@@ -22,8 +37,8 @@ class bigram(unigram):
 train_l, test_l = pp.load_data("G://CUNY/NLP/Assignments/HW1/", "train.txt", "test.txt")
 train_tkn_l = pp.token(train_l, test_l)
 V = pp.creat_vocabulary(train_tkn_l)
-uni = unigram(train_tkn_l)
-uni.fit()
-print(uni.unigramDict)
+bi = bigram(train_tkn_l)
+bi.fit()
+print(bi.p)
 
 
